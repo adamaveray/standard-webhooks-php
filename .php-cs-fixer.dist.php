f09ae19114c1c7b894efbacb51adb107d78939a3
Adam Averay <adam@averay.com>
2026-05-27 15:04:11 +0100

    Disable conflicting formatting rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,6 +11,16 @@ $finder = (new Finder())->exclude(['node_modules', 'vendor'])->in(__DIR__);
 return (new Config())
   ->setRules([
     '@PER-CS' => true,
-    '@PHP84Migration' => true,
+    '@PHP8x4Migration' => true,
+
+    // Defer to Prettier
+    'braces_position' => false,
+    'class_definition' => false,
+    'method_argument_space' => false,
+    'new_expression_parentheses' => false,
+    'no_extra_blank_lines' => false,
+    'operator_linebreak' => false,
+    'single_line_empty_body' => false,
   ])
+  ->setIndent('  ') // Match Prettier config
   ->setFinder($finder);
